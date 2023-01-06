2c3e6cd3108464ddba6b3d9e038e7fcc3c89e04e
Riddhi Patel <Ridshah2@gmail.com>
2023-01-06 09:55:03 -0500

    made changes

--- app.js
+++ app.js
@@ -26,31 +26,33 @@ for (let i = 1; i <= 100; i++) {
 }
 // Exercise 3 Section
 
-let x = 1;
+ i = 1;
 
-while (x <= 100) {
-    if (x % 2 == 0) {
-    console.log(x);
+while (i <= 100) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FIZZBUZZ");
+    } else if (i % 3 == 0) {
+            console.log("FIZZ");
+    } else if (i % 5 == 0) {
+            console.log("BUZZ") 
     }
-x++
+i++;
 }
 
-let i = 1;
-do (i <= 100) {
+i = 1;
+
+do {
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log(i,"FIZZBUZZ");
+        console.log("FIZZBUZZ");
     } else if (i % 3 == 0) {
-         console.log(i, "FIZZ");
+            console.log("FIZZ");
     } else if (i % 5 == 0) {
-         console.log(i, "BUZZ");
+            console.log("BUZZ") 
     }
-       i++;
-    } while (i <= 100);
+    i++;
+} while (i <= 100);
 
-
-
-
-
+// Exercise 4 section
 
 let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
 let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
@@ -65,8 +67,7 @@ console.log("did not find value");
     } 
 }
 
-
-
+// Exercise 5
 
 let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
 let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
@@ -75,10 +76,10 @@ let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number
 
 for (let i = start; i <= end; i++) {
     if(i % fizzDivisor == 0 && i % buzzDivisor == 0){
-    console.log("FIZZBUZZ");
-} else if (i, fizzDivisor == 0) {
-    console.log("FIZZ");
-} else if(i, bussDivisor == 0){
-    console.log("BUZZ");
+    console.log(i, "FIZZBUZZ");
+} else if (i % fizzDivisor == 0) {
+    console.log(i,"FIZZ");
+} else if(i % buzzDivisor == 0) {
+    console.log(i,"BUZZ");
 }
 }
